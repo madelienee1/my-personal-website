5fd1e1e6a678a36fbd9d29501163b2bd42583d62
Madeliene <madelienestiller@gmail.com>
2023-09-27 12:59:58 +1000

    small changes and portfolio section added

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const h2 = document.createElement("h2");
-// h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+//refactor code to store element nodes as variables.
+
+function letsConnectClick(event) {
+    document.querySelector("div.popUp").classList.remove("hidden")
+    document.querySelector("div.popUpContainer").classList.remove("hidden")
+    
+    console.log(event)
+}
+
+//this is a queryselector, it targets the button with the id of letsConnect, it then adds an event listener that "listens" or "watches" for 
+//when the button is clicked.
+//when the button is clicked it then fires the function we have created and passed as the second argument to the event listener.
+
+document.querySelector("button#letsConnect").addEventListener("click",letsConnectClick)
+
+
+function hidePopUp(event) {
+    document.querySelector("div.popUpContainer").classList.add("hidden")
+    console.log("hello")
+}
+
+
+document.querySelector(".popUpContainer").addEventListener("click",hidePopUp)
