0b51165e1bf7148d22e1d8d60cd340381467d62b
Madeliene <madelienestiller@gmail.com>
2023-09-29 19:19:09 +1000

    completed personal website

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-//refactor code to store element nodes as variables.
-
 function letsConnectClick(event) {
     document.querySelector("div.popUp").classList.remove("hidden")
     document.querySelector("div.popUpContainer").classList.remove("hidden")
@@ -8,9 +5,6 @@ function letsConnectClick(event) {
     console.log(event)
 }
 
-//this is a queryselector, it targets the button with the id of letsConnect, it then adds an event listener that "listens" or "watches" for 
-//when the button is clicked.
-//when the button is clicked it then fires the function we have created and passed as the second argument to the event listener.
 
 document.querySelector("button#letsConnect").addEventListener("click",letsConnectClick)
 
@@ -19,6 +13,5 @@ function hidePopUp(event) {
     document.querySelector("div.popUpContainer").classList.add("hidden")
     console.log("hello")
 }
-
 
 document.querySelector(".popUpContainer").addEventListener("click",hidePopUp)
