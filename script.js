5c2352c227f5095ac170a3f145e2d7631410d83f
Madeliene <madelienestiller@gmail.com>
2023-10-04 10:31:18 +1000

    website completed

--- script.js
+++ script.js
@@ -14,4 +14,29 @@ function hidePopUp(event) {
     console.log("hello")
 }
 
-document.querySelector(".popUpContainer").addEventListener("click",hidePopUp)
+document.querySelector(".popUpContainer").addEventListener("click", hidePopUp)
+
+// Map
+
+let map;
+
+async function initMap() {
+  const position = { lat: -26.652, lng: 153.082 };
+
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 12,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Maroochydore",
+  });
+}
+
+initMap();
